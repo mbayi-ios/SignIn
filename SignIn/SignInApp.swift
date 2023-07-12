6fe03104b056d0bedc804b24cb18bb47ae3c6047
Ambrose Mbayi <91916741+mbayi-ios@users.noreply.github.com>
2023-07-12 13:25:32 +0300

    Initial Commit

---
+++ SignIn/SignInApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInApp.swift
+//  SignIn
+//
+//  Created by Ambrose Mbayi on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SignInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
